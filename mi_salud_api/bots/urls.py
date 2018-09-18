36ec9a4cda10e64cd164a26d5a30e768c320e2dc
Frank <akura11.tt@gmail.com>
2018-09-18 18:34:03 -0500

    Ajustes: Endpoint y flag para fase de entrenamiento urls

--- mi_salud_api/bots/urls.py
+++ mi_salud_api/bots/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views.api import record_response_tag, tag_message_with_model
+from .views.api import model_is_in_training, tag_message_with_model
 
 
 urlpatterns = [
     path('tag-new-message/bot/<int:id_model>/', tag_message_with_model),
-    path('model-is-in-traning/bot/<int:id_model>/', tag_message_with_model)
+    path('model-is-in-traning/bot/<int:id_model>/', model_is_in_training)
 ]
